9d5558a4bd19447b58ed0343168527aa16992836
jabezinu <jabezzinu31@gmail.com>
2025-06-19 01:24:50 +0300

    remove unecessary code

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,8 @@ app.use(cors());
 app.use('/api/clients', clientRoutes);
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch(error => console.error('Database connection error:', error));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    }).catch(error => console.error('Database connection error:', error));
